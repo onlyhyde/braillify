76a85fe8994237127ad9cdeb41d49d2cb1a62b88
owjs3901 <owjs3901@gmail.com>
2025-11-24 21:16:27 +0900

    Fix infra

--- libs/braillify/build.rs
+++ libs/braillify/build.rs
@@ -1,10 +1,15 @@
 #[cfg(windows)]
 use embed_manifest::{embed_manifest, new_manifest};
 
-#[cfg(windows)]
 fn main() {
-    embed_manifest(new_manifest("Braillify.Braillify")).expect("unable to embed manifest file");
-}
+    // wasm 타겟으로 빌드할 때는 build.rs를 건너뜀
+    let target = std::env::var("TARGET").unwrap_or_default();
+    if target.contains("wasm32") {
+        return;
+    }
 
-#[cfg(not(windows))]
-fn main() {}
+    #[cfg(windows)]
+    {
+        embed_manifest(new_manifest("Braillify.Braillify")).expect("unable to embed manifest file");
+    }
+}
